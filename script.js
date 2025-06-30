6995fe65e9f3f8cd76855771f1ef29b1874cf728
elorapeter <florenceonyi09@gmail.com>
2025-06-30 16:51:15 +0100

    omo please work

--- script.js
+++ script.js
@@ -1826,7 +1826,7 @@ async function submitFeedback() {
   }
 
   try {
-    const response = await fetch('https://momentum-git-main-eloras-projects-da0df9d6.vercel.app/', {
+    const response = await fetch('https://momentum-git-main-eloras-projects-da0df9d6.vercel.app/api/send-feedback', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ feedback })
